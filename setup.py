90471a43ce39fe1b67c1aef35f24e917af1fa387
Jinyu Xie <xjygr08@gmail.com>
2018-09-10 22:47:22 -0400

    add long description into pypi

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 from setuptools import setup
 
-
-setup(name='simglucose',
-      version='0.1.9',
-      description='A Type-1 Diabetes Simulator as a Reinforcement Learning Environment in OpenAI gym or rllab (python implementation of UVa/Padova Simulator)',
-      url='https://github.com/jxx123/simglucose',
-      author='Jinyu Xie',
-      author_email='xjygr08@gmail.com',
-      license='MIT',
-      packages=['simglucose'],
-      install_requires=[
-          'pandas',
-          'numpy',
-          'scipy',
-          'matplotlib',
-          'pathos',
-          'gym==0.9.4'
-      ],
-      include_package_data=True,
-      zip_safe=False)
+setup(
+    name='simglucose',
+    version='0.2.1',
+    description=
+    'A Type-1 Diabetes Simulator as a Reinforcement Learning Environment in OpenAI gym or rllab (python implementation of UVa/Padova Simulator)',
+    url='https://github.com/jxx123/simglucose',
+    author='Jinyu Xie',
+    author_email='xjygr08@gmail.com',
+    license='MIT',
+    packages=['simglucose'],
+    install_requires=[
+        'pandas', 'numpy', 'scipy', 'matplotlib', 'pathos', 'gym==0.9.4'
+    ],
+    include_package_data=True,
+    zip_safe=False,
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown")
