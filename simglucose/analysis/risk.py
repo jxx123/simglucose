c7ec64907490a167ad58f489e2129179c34e080c
Andriy Drozdyuk <andriy@drozdyuk.com>
2023-10-27 13:49:43 +0000

    Explain risk limits

--- simglucose/analysis/risk.py
+++ simglucose/analysis/risk.py
@@ -11,6 +11,16 @@ def risk_index(BG, horizon):
     return (LBGI, HBGI, RI)
 
 def risk(BG):
+    """
+    Risk is to be a percentage - ranging from 0 to 100%.
+    The 20 and 600 mg/dl are just the values to which the risk formula was fit. 
+    The aim is to make the risk maximum when it is either 20 or 600.
+    The units in the paper below are different (mmol/l), but in our units (mg/dl) these limits are 20 and 600.
+
+    Reference, in particular see appendix for the derivation of risk:
+    https://diabetesjournals.org/care/article/20/11/1655/21162/Symmetrization-of-the-Blood-Glucose-Measurement
+
+    """
     MIN_BG = 20.0
     MAX_BG = 600.0
     if BG <= MIN_BG: 
