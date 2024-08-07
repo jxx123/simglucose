fa81bafedd46802ba34be17c8441beb2308c8210
Jinyu Xie <xjygr08@gmail.com>
2024-08-06 22:30:02 -0700

    udpate pump restriction unit bug

--- simglucose/actuator/pump.py
+++ simglucose/actuator/pump.py
@@ -4,12 +4,12 @@ import logging
 import numpy as np
 
 INSULIN_PUMP_PARA_FILE = pkg_resources.resource_filename(
-    'simglucose', 'params/pump_params.csv')
+    "simglucose", "params/pump_params.csv"
+)
 logger = logging.getLogger(__name__)
 
 
 class InsulinPump(object):
-    U2PMOL = 6000
 
     def __init__(self, params):
         self._params = params
@@ -21,23 +21,20 @@ class InsulinPump(object):
         return cls(params)
 
     def bolus(self, amount):
-        bol = amount * self.U2PMOL  # convert from U/min to pmol/min
-        bol = np.round(bol / self._params['inc_bolus']
-                       ) * self._params['inc_bolus']
-        bol = bol / self.U2PMOL     # convert from pmol/min to U/min
-        bol = min(bol, self._params['max_bolus'])
-        bol = max(bol, self._params['min_bolus'])
+        # inc_bolus and max_bolus are in U
+        bol = np.round(amount / self._params["inc_bolus"]) * self._params["inc_bolus"]
+        bol = min(bol, self._params["max_bolus"])
+        bol = max(bol, self._params["min_bolus"])
         return bol
 
     def basal(self, amount):
-        bas = amount * self.U2PMOL  # convert from U/min to pmol/min
-        bas = np.round(bas / self._params['inc_basal']
-                       ) * self._params['inc_basal']
-        bas = bas / self.U2PMOL     # convert from pmol/min to U/min
-        bas = min(bas, self._params['max_basal'])
-        bas = max(bas, self._params['min_basal'])
+        bas = amount * 60.0  # convert from U/min to U/h
+        bas = np.round(bas / self._params["inc_basal"]) * self._params["inc_basal"]
+        bas = bas / 60.0  # convert from U/h to U/min
+        bas = min(bas, self._params["max_basal"])
+        bas = max(bas, self._params["min_basal"])
         return bas
 
     def reset(self):
-        logger.info('Resetting insulin pump ...')
+        logger.info("Resetting insulin pump ...")
         pass
